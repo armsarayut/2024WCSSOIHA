ae44ca59b24c3d62a7cd991d9febea87efdf20d8
armsarayut <sarayut.buttathai@gmail.com>
2023-06-27 17:46:25 +0700

    EnDecrypt 202307271746

--- Data/ConnGlobals.cs
+++ Data/ConnGlobals.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using System.IO;
 using Org.BouncyCastle.Crypto.Engines;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace GoWMS.Server.Data
 {
@@ -25,8 +27,8 @@ namespace GoWMS.Server.Data
         //private static readonly string NpgDB = "Gowes"; // Production
 
         private static readonly string NpgPort = "5434";
-        
-        private static readonly string NpgUser = "postgres";
+
+        private static readonly string NpgUser = "postgres"; // "postgres";
         private static readonly string NpgPass = "@ei0u2020";
         //private static readonly string NpgPass = "@ei0u";
         private static readonly string NpgContime = "120";
@@ -49,6 +51,11 @@ namespace GoWMS.Server.Data
             string _DbQAname = string.Empty;
             string _HostQAmode = string.Empty;
 
+
+            string _Dbkey = string.Empty;
+            string _Dbmainkey = string.Empty;
+            string _DbPass = string.Empty;
+
             var configurationBuilder = new ConfigurationBuilder();
             var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
             configurationBuilder.AddJsonFile(path, false);
@@ -64,24 +71,37 @@ namespace GoWMS.Server.Data
             _DbQAname = root.GetSection("DBHost").GetSection("NameQA").Value;
             _HostQAmode = root.GetSection("DBHost").GetSection("HostModeQA").Value;
 
+            _Dbkey = root.GetSection("Appkey").GetSection("Keys").Value;
+            _Dbmainkey = root.GetSection("Appkey").GetSection("Users").Value;
+
+
+            //var encrypted = EncryptString(_Dbmainkey, _Dbkey);
+
+            var decrypted = DecryptString(_Dbmainkey, _Dbkey);
+
+
+
 
             bool bLocal = true;
 
             if (_Hostmode== "1")
             {
                 bLocal = false;
-            }
+                _DbPass = decrypted;
+            }else
+            { _DbPass = NpgPass;
+                    }
             
 
             if (bLocal)
             {
                 //return "Server=" + NpgServer + " ;Port=" + NpgPort + ";Database=" + NpgDB + ";User Id=" + NpgUser + ";Password=" + NpgPass + ";Timeout=" + NpgContime + ";";
 
-                return "Server=" + _IPQAString + " ;Port=" + _PortQAString + ";Database=" + _DbQAname + ";User Id=" + NpgUser + ";Password=" + NpgPass + ";Timeout=" + NpgContime + ";";
+                return "Server=" + _IPQAString + " ;Port=" + _PortQAString + ";Database=" + _DbQAname + ";User Id=" + NpgUser + ";Password=" + _DbPass + ";Timeout=" + NpgContime + ";";
             }
             else
             {
-                return "Server=" + _IPString + " ;Port=" + _PortString + ";Database=" + _Dbname + ";User Id=" + NpgUser + ";Password=" + NpgPass + ";Timeout=" + NpgContime + ";";
+                return "Server=" + _IPString + " ;Port=" + _PortString + ";Database=" + _Dbname + ";User Id=" + NpgUser + ";Password=" + _DbPass + ";Timeout=" + NpgContime + ";";
 
              
             }
@@ -137,6 +157,72 @@ namespace GoWMS.Server.Data
             //return "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=" + oraServer + ")(PORT=" + oraPort + ")))(CONNECT_DATA=(SERVER=DEDICATED)(SID = " + oraDB + ")));User Id=" + oraUser + ";Password=" + oraPass + ";";
             
             return "Provider = OraOLEDB.Oracle; Data Source = " + oraUser +"; User Id = " + oraUser +"; Password = "+ oraPass+"; OLEDB.NET = True;";
+        }
+        #endregion
+
+        #region Encryp Decrypt
+        public static string EncryptString(string text, string keyString)
+        {
+            var key = Encoding.UTF8.GetBytes(keyString);
+
+            using (var aesAlg = Aes.Create())
+            {
+                using (var encryptor = aesAlg.CreateEncryptor(key, aesAlg.IV))
+                {
+                    using (var msEncrypt = new MemoryStream())
+                    {
+                        using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                        using (var swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(text);
+                        }
+
+                        var iv = aesAlg.IV;
+
+                        var decryptedContent = msEncrypt.ToArray();
+
+                        var result = new byte[iv.Length + decryptedContent.Length];
+
+                        Buffer.BlockCopy(iv, 0, result, 0, iv.Length);
+                        Buffer.BlockCopy(decryptedContent, 0, result, iv.Length, decryptedContent.Length);
+
+                        return Convert.ToBase64String(result);
+                    }
+                }
+            }
+        }
+
+
+        public static string DecryptString(string cipherText, string keyString)
+        {
+            var fullCipher = Convert.FromBase64String(cipherText);
+
+            var iv = new byte[16];
+            var cipher = new byte[16];
+
+            Buffer.BlockCopy(fullCipher, 0, iv, 0, iv.Length);
+            Buffer.BlockCopy(fullCipher, iv.Length, cipher, 0, iv.Length);
+            var key = Encoding.UTF8.GetBytes(keyString);
+
+            using (var aesAlg = Aes.Create())
+            {
+                using (var decryptor = aesAlg.CreateDecryptor(key, iv))
+                {
+                    string result;
+                    using (var msDecrypt = new MemoryStream(cipher))
+                    {
+                        using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                        {
+                            using (var srDecrypt = new StreamReader(csDecrypt))
+                            {
+                                result = srDecrypt.ReadToEnd();
+                            }
+                        }
+                    }
+
+                    return result;
+                }
+            }
         }
         #endregion
     }
